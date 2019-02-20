3135764e77b27998848f2dbe1e57e54726e0cc6d
Salah Osman <osman144@umn.edu>
2019-02-20 10:11:07 -0600

    added more elements from scraped html

--- scraper.rb
+++ scraper.rb
@@ -10,9 +10,21 @@ parsed_content = Nokogiri::HTML(content)
 
 parsed_content.css('.content').css('.rows').css('.result-row').each do |row|
     title = row.css('.result-title').inner_text
+    posted_at = row.css('.result-info').css('.result-date').first.attributes['datetime'].value
+    neighbor_elem = row.css('.result-info').css('.result-meta').css('.result-hood')
+    # link = 
+
+    if neighbor_elem.any? 
+        neighborhood = neighbor_elem.inner_text
+    else 
+        neighborhood = ''
+    end
+
     puts title 
-    puts '-------------------------------------'
+    puts "Posted at #{posted_at}"
+    puts "Neighborhood: #{neighborhood}"
+    puts '-------------------------------------------------------------'
 end 
 
 
-# puts parsed_content.css('.content').css('.rows').css('.result-row').css('.result-title').inner_text
+# puts parsed_content.css('.content').css('.rows').css('.result-row').css('.result-info').css('.result-meta').css('.result-hood')
