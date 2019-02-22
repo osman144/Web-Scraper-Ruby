70b9787156d14bbcf540fd1ae4b8fbe494e86abb
Salah Osman <osman144@umn.edu>
2019-02-21 20:29:43 -0600

    added search term values, cities value

--- scraper.rb
+++ scraper.rb
@@ -1,30 +1,39 @@
 require 'open-uri'
 require 'nokogiri'
 
-url = "https://minneapolis.craigslist.org/d/services/search/bbb"
+cities = ['Minneapolis']
+search_terms = ['']
 
-document = open(url)
-content = document.read
+cities.each do |city|
+    search_terms.each do |search_term|
 
-parsed_content = Nokogiri::HTML(content)
+        url = "https://#{city}.craigslist.org/search/sss?query=#{search_terms}&sort=rel"
 
-parsed_content.css('.content').css('.rows').css('.result-row').each do |row|
-    title = row.css('.result-title').inner_text
-    posted_at = row.css('.result-info').css('.result-date').first.attributes['datetime'].value
-    neighbor_elem = row.css('.result-info').css('.result-meta').css('.result-hood')
-    link = row.css('.result-title').first.attributes["href"].value
+        document = open(url)
+        content = document.read
 
-    if neighbor_elem.any? 
-        neighborhood = neighbor_elem.inner_text
-    else 
-        neighborhood = ''
+        parsed_content = Nokogiri::HTML(content)
+
+        parsed_content.css('.content').css('.rows').css('.result-row').each do |row|
+            title = row.css('.result-title').inner_text
+            posted_at = row.css('.result-info').css('.result-date').first.attributes['datetime'].value
+            neighbor_elem = row.css('.result-info').css('.result-meta').css('.result-hood')
+            link = row.css('.result-title').first.attributes["href"].value
+
+            if neighbor_elem.any? 
+                neighborhood = neighbor_elem.inner_text
+            else 
+                neighborhood = ''
+            end
+
+            puts title 
+            puts "Posted at #{posted_at}"
+            puts "Neighborhood: #{neighborhood}"
+            puts "Link:  #{link} "
+            puts '-------------------------------------------------------------'
+        end 
+
     end
-
-    puts title 
-    puts "Posted at #{posted_at}"
-    puts "Neighborhood: #{neighborhood}"
-    puts "Link:  #{link} "
-    puts '-------------------------------------------------------------'
 end 
 
 
